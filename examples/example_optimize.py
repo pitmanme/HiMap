c910280ba6846ff2f304b9d88a7c0c445fba83b3
M Pitman <49458516+pitmanme@users.noreply.github.com>
2022-05-19 16:43:20 -0700

    Add files via upload
    Updated version

--- examples/example_optimize.py
+++ examples/example_optimize.py
@@ -1,15 +1,14 @@
 import lomap
-import sys
-import networkx as nx
-import numpy as np
-import csv
-import sys
 
-##################################################################################################
-# This example file walks through how to read in molecules with Lomap using an interactive
-# clustering mode. To select defaults during clustering, simply hit enter or type the displayed
-# options. The user selected clusters will then be sent for design optimization. 
-##################################################################################################
+"""
+This example file walks through how to read in molecules with LOMAP and then
+determine optimal FEP designs. This uses an interactive clustering mode.
+To select defaults during clustering, hit enter or type the displayed
+options. The user selected clusters will then be sent for design optimization.
+"""
+# *****************************************************************************
+# This example file was written by Dr. Mary Pitman. 2022
+# *****************************************************************************
 
 #-------------------------------------------------------#
 # Generate similarity scores.
@@ -24,37 +23,24 @@ strict, loose = db_mol.build_matrices()
 sim_np = strict.to_numpy_2D_array()
 
 # Clean data if Lomap produces rare error. If score is NaN, replace with 0.0
-n_arr = np.where(np.isnan(sim_np), 0.0, sim_np)
+n_arr = lomap.clean_NaN(sim_np)
 
 #-------------------------------------------------------#
 # Clustering.
 #-------------------------------------------------------#
-# Generate distance array.
-X = 1 - n_arr
+# Create ID_list from db_mol prior to clustering.
+ID_list = lomap.db_mol_IDs(db_mol, n_arr)
 
-# Create ID_list from dbmols prior to clustering. To be incorporated.
-mol_names = ['ID']
-for i in range(n_arr.shape[1]):
-    fname = db_mol[i].getName()
-    fsplit = fname.split(".", 1)[0]
-    mol_names.append(fsplit)
-# Convert the name list to an np array.
-mol_arr_pre = np.array(list(mol_names))
-# Cleave the 'ID' entry
-ID_list = mol_arr_pre[1:]
-
-
-# Perform clustering.
-labels, cluster_kwarg = lomap.cluster_interactive(X, ID_list)
-
-# Generate sub-arrays of clusters. Stored in dictionaries.
-sub_arr, sub_ID = lomap.sub_arrays(labels, n_arr, ID_list)
+# Perforrm clustering.
+#   sub_arr, sub_ID:   the np_arr and ID_list subdivided by clusters
+#   selected_clusters: user selected clusters during interaction.
+sub_arr, sub_ID, selected_clusters = lomap.cluster_interactive(n_arr, ID_list)
 
 #-------------------------------------------------------#
 # Optimization.
 #-------------------------------------------------------#
-# Example reference ligands.
+# Example reference ligand.
 ref_ligs = ['ejm_31']
 
 # Send the user selected clusters for optimization.
-lomap.clusters2optimize(sub_arr, sub_ID, clusters2optim = cluster_kwarg, ref_ligs=ref_ligs)
+lomap.clusters2optimize(sub_arr, sub_ID, clusters2optim = selected_clusters, ref_ligs=ref_ligs)
